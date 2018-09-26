56e2b01d562a8b7dd5fa8e2cb7e2bfff161e31e9
asianvader <phoebe.voong@gmail.com>
2018-09-27 00:29:26 +0100

    hide collapsible navbar after click

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,7 @@
-const phoebe = "phoebe";
+// hide collapsible navbar after click
 
-function orbitalPeriod(arr) {
-  const GM = 398600.4418;
-  const earthRadius = 6367.4447;
-
-  let newArr = Array.from(arr);
-
-  for (let i = 0; i < newArr.length; i++) {
-      let radius = Math.pow((arr[i].avgAlt + earthRadius), 3);
-  let areaOfCircumference = 4 * (Math.pow((Math.PI), 2));
-  let totalRadius = radius * areaOfCircumference;
-  let time = Math.round(Math.sqrt(totalRadius/GM));
-    delete newArr[i].avgAlt;
-    newArr[i].orbitalPeriod = time;
-  }
-
-  return newArr;
-}
-
-//orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
-console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]));
+$(document).ready(function () {
+  $('.navbar-nav>li>a').on('click', function(){
+      $('.navbar-collapse').collapse('hide');
+  });
+});
