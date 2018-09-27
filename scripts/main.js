a0cce5b4961ce3531e378c846a21a9ab3e65e293
asianvader <phoebe.voong@gmail.com>
2018-09-27 15:33:18 +0100

    replace jQuery CDN link with minified version. add smooth scrolling to navbar.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,28 @@
-// hide collapsible navbar after click
 
 $(document).ready(function () {
+  // hide collapsible navbar after click
   $('.navbar-nav>li>a').on('click', function(){
       $('.navbar-collapse').collapse('hide');
   });
+
+  // Smooth scrolling
+  var scrollLink = $('.scroll');
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
+
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+      scrollLink.each(function() {
+      var sectionOffset = $(this.hash).offset().top - 20;
+        if ( sectionOffset <= scrollbarLocation ) {
+          $(this).parent().addClass('active');
+          $(this).parent().siblings().removeClass('active');
+      }
+    })
+  });
+
 });
