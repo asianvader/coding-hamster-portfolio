736684c8f74c5e3b46a39866c3efa950781c9c09
asianvader <phoebe.voong@gmail.com>
2018-10-04 23:13:34 +0100

    correct jQuery objects.

--- scripts/main.js
+++ scripts/main.js
@@ -15,10 +15,10 @@ $(document).ready(function () {
   });
 
   $(window).scroll(function() {
-    var $scrollbarLocation = $(this).scrollTop();
-      scrollLink.each(function() {
+    var scrollbarLocation = $(this).scrollTop();
+      $scrollLink.each(function() {
       var sectionOffset = $(this.hash).offset().top - 20;
-        if ( sectionOffset <= $scrollbarLocation ) {
+        if ( sectionOffset <= scrollbarLocation ) {
           $(this).parent().addClass('active');
           $(this).parent().siblings().removeClass('active');
       }
