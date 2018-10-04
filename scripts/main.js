20035c3b562ba529c473db63e605f3b7a4d7950c
asianvader <phoebe.voong@gmail.com>
2018-10-04 12:00:21 +0100

    changed variables to jQuery objects.

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
   });
 
   // Smooth scrolling
-  var scrollLink = $('.scroll');
-  scrollLink.click(function(e) {
+  var $scrollLink = $('.scroll');
+  $scrollLink.click(function(e) {
     e.preventDefault();
     $('body,html').animate({
       scrollTop: $(this.hash).offset().top - 66
@@ -15,15 +15,14 @@ $(document).ready(function () {
   });
 
   $(window).scroll(function() {
-    var scrollbarLocation = $(this).scrollTop();
+    var $scrollbarLocation = $(this).scrollTop();
       scrollLink.each(function() {
       var sectionOffset = $(this.hash).offset().top - 20;
-        if ( sectionOffset <= scrollbarLocation ) {
+        if ( sectionOffset <= $scrollbarLocation ) {
           $(this).parent().addClass('active');
           $(this).parent().siblings().removeClass('active');
       }
     })
   });
-
 
 });
